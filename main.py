023fa84a3c6963354edacd347fae96fe168b20bd
Aksel Yusein <akselyusein@Aksels-MacBook-Pro.local>
2025-12-01 23:05:22 +0000

    FDF-06

--- main.py
+++ main.py
@@ -3,28 +3,32 @@ from flight_search import FlightSearch
 from flight_data import FlightData
 from datetime import datetime, timedelta
 
-date = datetime.now().date() + timedelta(days=50)
-formatted = str(date)
+def main():
+    date = datetime.now().date() + timedelta(days=50)
+    formatted = str(date)
 
-date_format = formatted
+    date_format = formatted
 
-sheety_data = DataManager()
-flight_data = FlightSearch(date=date_format)
+    sheety_data = DataManager()
+    flight_data = FlightSearch(date=date_format)
 
-cities = sheety_data.cities()
+    cities = sheety_data.cities()
 
-iata_codes = flight_data.search_for_iata(cities)
+    iata_codes = flight_data.search_for_iata(cities)
 
-spreadsheet = sheety_data.get_data()
+    spreadsheet = sheety_data.get_data()
 
-try:
-    final = flight_data.flight_offer(spreadsheet, is_direct="false")
-except KeyError:
-    sheety_data.update_data(iata_codes)
-    updated_spreadsheet = sheety_data.get_data()
-    updated_final = flight_data.flight_offer(updated_spreadsheet, is_direct="false")
-    new_data = FlightData(updated_final)
-    new_data.get_flights()
-else:
-    data = FlightData(final)
-    data.get_flights()
+    try:
+        final = flight_data.flight_offer(spreadsheet, is_direct="false")
+    except KeyError:
+        sheety_data.update_data(iata_codes)
+        updated_spreadsheet = sheety_data.get_data()
+        updated_final = flight_data.flight_offer(updated_spreadsheet, is_direct="false")
+        new_data = FlightData(updated_final)
+        new_data.get_flights()
+    else:
+        data = FlightData(final)
+        data.get_flights()
+
+if __name__ == "__main__":
+    main()
